1774edee9dd194db8def7f1e799b92c762e8c524
Racknar0 <racknarow1@gmail.com>
2022-02-25 17:29:06 -0500

    Add listeners

--- js/app.js
+++ js/app.js
@@ -1,10 +1,46 @@
-// Variables
+/************** ( '// Campos del form' ) **************/
 
-const mascotaInput = document.querySelector('#mascota');
-const propiertarioInput = document.querySelector('#propiertario');
-const telefonoInput = document.querySelector('#telefono');
-const fechaInput = document.querySelector('#fecha');
-const horaInput = document.querySelector('#hora');
-const sintomasInput = document.querySelector('#sintomas');
+const mascotaInput = document.querySelector("#mascota");
+const propietarioInput = document.querySelector("#propietario");
+const telefonoInput = document.querySelector("#telefono");
+const fechaInput = document.querySelector("#fecha");
+const horaInput = document.querySelector("#hora");
+const sintomasInput = document.querySelector("#sintomas");
 
-const formulario = document.querySelector('#nueva-cita');
+
+/************** ( '//UI' ) **************/
+const formulario = document.querySelector("#nueva-cita");
+const contenedorCitas = document.querySelector("#citas");
+
+
+/************** ( 'RESITRAR EVENTOS' ) **************/
+eventListeners();
+
+function eventListeners() {
+  mascotaInput.addEventListener("input", datosCita);
+  propietarioInput.addEventListener("input", datosCita);
+  telefonoInput.addEventListener("input", datosCita);
+  fechaInput.addEventListener("input", datosCita);
+  horaInput.addEventListener("input", datosCita);
+  sintomasInput.addEventListener("input", datosCita);
+}
+
+/************** ( 'OBJETO CON INFO DE LAS CITAS' ) **************/
+const citaObj = {
+  masota: "",
+  propietario: "",
+  telefono: "",
+  fecha: "",
+  hora: "",
+  sintomas: "",
+};
+
+
+/************** ( 'FUCNCIONES' ) **************/
+
+//agrega datpos de la cita
+function datosCita(e) {
+  citaObj[e.target.name] = e.target.value;
+
+  console.log(citaObj);
+}
