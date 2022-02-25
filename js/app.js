d8ba46bcc06c649204bdc45a2157c24e618eb935
Racknar0 <racknarow1@gmail.com>
2022-02-25 17:14:08 -0500

    Upload template

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Variables
+
+const mascotaInput = document.querySelector('#mascota');
+const propiertarioInput = document.querySelector('#propiertario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#nueva-cita');
